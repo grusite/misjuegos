0962c23121a73a6c294c5fcd840dfb4a4842af11
Jorge <grusite@gmail.com>
2021-10-29 01:13:44 +0200

    enhanced linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,13 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: ['plugin:vue/essential', 'plugin:prettier/recommended', 'plugin:jest/recommended'],
+  extends: ['plugin:vue/essential', 'plugin:jest/recommended'],
   parserOptions: {
     ecmaVersion: 12,
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
   },
   plugins: ['vue', '@typescript-eslint'],
-  rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto',
-      },
-    ],
-  },
   settings: {
     'import/resolver': {
       typescript: {},
