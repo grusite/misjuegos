20e116758e48661214ad7871cd34512bf87ccfac
Jorge Martín <grusite@gmail.com>
2022-01-08 23:50:11 +0100

    config: Add Pinia to manage store

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,30 +1,14 @@
-import { reactive, computed, readonly, toRef } from 'vue'
+import { defineStore } from 'pinia'
 
-interface State {
-  count: number
-  other: string
-}
-
-const state = reactive<State>({
-  count: 1,
-  other: '',
-})
-
-const actions = {
-  increment: () => {
-    state.count++
+export const useStore = defineStore('main', {
+  state: () => {
+    return { count: 0 }
   },
-}
-
-const getters = {
-  isEven: computed(() => {
-    return state.count % 2 == 0
-  }),
-}
-
-export const store = {
-  state: readonly(state),
-  actions,
-  getters,
-  ref: <K extends keyof State>(key: K) => toRef(state, key),
-}
+  // could also be defined as
+  // state: () => ({ count: 0 })
+  actions: {
+    increment() {
+      this.count++
+    },
+  },
+})
